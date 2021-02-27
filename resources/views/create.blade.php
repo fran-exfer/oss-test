85803f5d4fc49a179e3c3f98d77b8da88a767858
fran-exfer <fran.exfer@gmail.com>
2021-02-27 01:55:56 +0100

    Add create image form.

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -20,7 +20,51 @@
 @endsection
 
 @section ('content')
-    
-    
+
+    <div class="columns is-centered">
+        <div class="column is-6">
+            <article class="box">
+        
+                <form action="{{ route('store') }}" method="POST">
+                    @csrf
+
+                    <div class="columns">
+                        <div class="column is-8 field">
+                            <label class="label">Title</label>
+                            <div class="control">
+                                <input class="input" type="text" id="title" name="title" placeholder="Image title">
+                            </div>
+                        </div>
+
+                        <div class="column field">
+                            <label class="label">Category</label>
+                            <div class="control">
+                                <input class="input" type="text" id="category" name="category" placeholder="Image category">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label class="label">Description</label>
+                        <div class="control">
+                            <textarea class="textarea" type="text" id="description" name="description" placeholder="Image description"></textarea>
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label class="label">Image URL</label>
+                        <div class="control">
+                            <input class="input" type="text" id="url" name="url" placeholder="Image url">
+                        </div>
+                    </div>
+
+                    <div class="field buttons is-centered p-4">
+                        <button type="submit" class="button is-success">Add new image</button>
+                    </div>
+                </form>
+
+            </article>
+        </div>
+    </div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ImagesController::class, 'index'])->name('index');
 Route::get('/create', [ImagesController::class, 'create'])->name('create');
+Route::post('/', [ImagesController::class, 'store'])->name('store');
